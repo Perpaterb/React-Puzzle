7e942254174b0ef09157a02a3ec837dc55660d50
“dwbra” <dworkmang@gmail.com>
2021-06-10 11:12:09 +1000

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
+import Draggable from './components/Draggable';
 
 function App() {
+  const mainStyles = {
+    backgroundColor: "red",
+    height: "100px",
+    width: "100px"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={mainStyles}>
+      <Draggable></Draggable>
     </div>
   );
 }
