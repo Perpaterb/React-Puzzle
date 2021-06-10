654e35712a77cf789fb2643fcf2148564adb3540
brianskiies <brian.he3@gmail.com>
2021-06-10 12:58:37 +1000

    added static boxes

--- src/components/styled-components/Gate.js
+++ src/components/styled-components/Gate.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components'
 
 const Gate = styled.div `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px;
     width: 30rem;
     height: 10rem;
     cursor: pointer;
